d3db8c9996acfe587be4f6c9232653ba629517eb
tlamart <t_lamart@hotmail.fr>
2020-07-09 20:50:54 +0200

    Updates

---
+++ precache-manifest.3a782ce1581a6e4fd191d2d76f1fd2b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37315794f428b7467c7101e9dc53a82b",
+    "url": "/rand-joke/index.html"
+  },
+  {
+    "revision": "8edb945131059fa61da2",
+    "url": "/rand-joke/static/css/main.48e8e758.chunk.css"
+  },
+  {
+    "revision": "c36772595baaebc4e7bf",
+    "url": "/rand-joke/static/js/2.49b6f1d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rand-joke/static/js/2.49b6f1d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8edb945131059fa61da2",
+    "url": "/rand-joke/static/js/main.d9dcf65e.chunk.js"
+  },
+  {
+    "revision": "69abcc86d08eb9ee41d0",
+    "url": "/rand-joke/static/js/runtime-main.d9d147b5.js"
+  }
+]);
